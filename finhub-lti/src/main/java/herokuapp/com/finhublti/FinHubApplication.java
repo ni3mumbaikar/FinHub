0348d7aa7469111f564a1a399157b93eebd69602
ni3mumbaikar <nitinkale035@gmail.com>
2022-07-19 08:13:06 +0530

    Simple Json response added on '/greeting' endpoint.

---
+++ finhub-lti/src/main/java/herokuapp/com/finhublti/FinHubApplication.java
@@ -0,0 +1,13 @@
+package herokuapp.com.finhublti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinHubApplication.class, args);
+	}
+
+}
